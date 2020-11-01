e5cdcf783e9d8212caf256777a89121ecf9e3fe9
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-11-01 15:19:37 +0100

    binary search of given list added

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,40 @@ public class Main {
         for (int item:table) {
             System.out.print(item + " ");
         }
+        int[] table2 = new int[]{0, 4, 6, 9, 10, 14, 15, 18, 19, 20};
+        int start = 0;
+        int end = table2.length;
+        Scanner value = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Following table is given: ");
+        for (int item:table2) System.out.print(item + " ");
+
+        System.out.println();
+        System.out.print("Which number are you looking for in given table?: ");
+        int requestedVal = value.nextInt();
+        lookForValue(requestedVal, start, end, table2);
     }
 
-    private static void tableTrans(int start, int end, int table[]) {
+    private static void lookForValue(int requestedVal, int start, int end, int[] tab) {
+
+        if (tab[end-1] < requestedVal || tab[start] > requestedVal)
+            System.out.println("This value was not found in that table...");
+        else if (tab[(start + end)/2] == requestedVal) {
+            System.out.println("This value is found in the table with the index of " + (start + end)/2);
+            System.exit(0);
+
+        }
+        else if (tab[(start + end)/2] > requestedVal) {
+            lookForValue(requestedVal, start, (start + end)/2, tab);
+        }
+        else if (tab[(start + end)/2] < requestedVal) {
+            lookForValue(requestedVal, (start + end)/2, end, tab);
+        }
+
+
+    }
+
+    private static void tableTrans(int start, int end, int[] table) {
         if (start < end) {
             int temp = table[start];
             table[start] = table [end];
